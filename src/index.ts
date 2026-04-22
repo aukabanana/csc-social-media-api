f0ef67f12b64944f780a51f0db0c1ef48550d2c9
aukabanana <hope25501@gmail.com>
2026-04-22 14:23:01 +0700

    feat: create and read users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
+import { z, ZodError } from "zod";
 import morgan from "morgan";
+import prisma from "./lib/prisma.js";
+import { REPLCommand } from "node:repl";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,10 +13,44 @@ if (!PORT) throw new Error('PORT is missing in your env file');
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (_req, res: Response) => {
-  res.send("Hello World");
+const userSchema = z.object({
+  username: z.string(),
+  email: z.email(),
 });
 
+const postSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean().default(false),
+  authorId: z.string(),
+});
+
+app.get("/", (_req, res: Response) => {
+  res.send("Hello World");
+});
+
+app.post('/users', async (req: Request, res: Response) => {
+  try {
+    const { username, email } = userSchema.parse(req.body);
+    const user = await prisma.user.create({
+      data: { username, email }
+    });
+
+    res.status(201).json(user);
+  } catch (e) {
+    console.log(e);
+    if (e instanceof ZodError)
+      return res.status(400).json(e.issues);
+
+    res.status(500).json(e);
+  }
+});
+
+app.get('/users', async (req: Request, res: Response) => {
+  const data = await prisma.user.findMany();
+  res.status(200).json(data);
+})
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
